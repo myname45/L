be434e5f8d65eb644fb6c5df0a390ee38ad4a028
myname45 <150045867+myname45@users.noreply.github.com>
2024-05-05 22:06:48 +0530

    Update expect_system.py

--- expect_system.py
+++ expect_system.py
@@ -1,45 +1,42 @@
 from random import choice
 
 class ExpertSystem:
-    def __init__(self, symptoms, deseases):
+    def __init__(self, symptoms, diseases):
         self.symptoms = symptoms
-        self.deseases = deseases
+        self.diseases = diseases
         self.current_symptom = None
-        self.current_desease = None
+        self.current_disease = None
     
     def start(self):
         self.current_symptom = choice(self.symptoms)
         n = int(input(f"What is severity of {self.current_symptom} (0-10) : "))
-        self.current_desease = self.deseases[self.current_symptom]
-        if n>5:
-            print(f"You may have {self.current_desease}")
+        self.current_disease = self.diseases[self.current_symptom]
+        if n > 5:
+            print(f"You may have {self.current_disease}")
         else:
-            print(f"You dont have {self.current_desease}")
+            print(f"You don't have {self.current_disease}")
             
         self.askNextQuestion()
     
     def askNextQuestion(self):
-        if self.current_desease is not None:
+        if self.current_disease is not None:
             n = input("Do you have any other symptoms (yes/no) : ")
             if n.lower() == "yes":
                 self.start()
             else:
-                print("Thank you for using our Expert System !")
+                print("Thank you for using our Expert System!")
         else:
-            print("Sorry, I cant Help you with desease dignosis.")
+            print("Sorry, I can't help you with disease diagnosis.")
             
 if __name__=='__main__':
     symptoms = ["fever", "cough", "sore throat", "running nose", "headache"]
-    desease = {
-        "fever":"flu",
-        "cough":"cold",
+    disease = {
+        "fever": "flu",
+        "cough": "cold",
         "sore throat": "strep throat",
         "running nose": "allergies",
         "headache": "migraine"
     }
             
-    es = ExpertSystem(symptoms, desease)
+    es = ExpertSystem(symptoms, disease)
     es.start()
-        
-        
-            
